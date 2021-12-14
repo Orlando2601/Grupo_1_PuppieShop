4ff934acdbfc291635cddd8cbd39f3b1f6aff89d
oswar <oswar.baez26@gmail.com>
2021-12-14 18:08:28 -0500

    [Para iniciar...]

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const expres = require('express');
+const express = require('express');
 const path = require('path');
 const app = express();
 const publicPath =path.resolve(__dirname, './public');
-app.use(express.publicPath(publicPath));
+app.use(express.static(publicPath));
+const port = 3030;
+
+app.listen(port, ()=>{console.log('Servidor corriendo en puerto ' + port);});
+app.get('/detalle-producto', (req, res)=>{res.sendFile(path.join(__dirname, '/views/detalle-producto.html'))});
