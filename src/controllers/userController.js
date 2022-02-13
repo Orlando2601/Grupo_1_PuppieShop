d16c7975254dd05acb90ff78bbd47c2153e0f5cf
oswar <oswar.baez26@gmail.com>
2022-02-13 07:19:42 -0500

    [Registro usuarios e imagen con errores en campos]

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const userFilePath = path.join(__dirname, '../data/user.json');
 const usuarios = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
+const { validationResult } = require('express-validator')
+const bcryptjs = require('bcryptjs')
 
 const userController ={
     
@@ -12,17 +14,37 @@ const userController ={
     registro:(req,res)=>{
         res.render('users/registro')
     },
-    users:(req,res)=>{
-      let nuevousuario = {
-        ...req.body,
-       
-        
-      };
-      usuarios.push(nuevousuario);
-      fs.writeFileSync(userFilePath,JSON.stringify(usuarios,null,' '));
+    users:(req, res)=>{
+      console.log("no hay archivo imagen" + req.file);
+      const errors = validationResult(req)
+      let newReference = usuarios.length
+      if(errors.isEmpty()){
+          let userInDB = usuarios.find(user => user.correo == req.body.correo);
+          if(userInDB === undefined && req.file){
+              
+              let nuevoUser = {
+                  id: newReference + 1,
+                  ...req.body,
+                  contraseña: bcryptjs.hashSync(req.body.contraseña, 10),
+                  imagen:req.file.filename
+              }
+              usuarios.push(nuevoUser)
+              fs.writeFileSync(userFilePath, JSON.stringify(usuarios, null, ' '))
+              res.redirect('/user/login') 
+          }else{
+              res.render('users/registro')
+              
+          }
+      }else{ 
+          
+          res.render('users/registro',{
+          
+              errors: errors.array(),
+              old: req.body
+          })
+      }
 
-      res.redirect('/');
-    }
+  }
     
 }
 
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,10 +2,47 @@
 const express = require('express'); /* Importamos modulo express */
 const router = express.Router(); /* Definimos el método Router de express a la variable router para exportarla */
 const userController = require('../controllers/userController');
+const path = require('path')
+const {body} = require('express-validator')
+const multer = require('multer')
 /* //////////////////////////////////////////////////////////////////////////////////////////// */
+
+/* VALIDACIONES DE CAMPOS //////////////////////////////////////////////////////////////////*/
+const validaciones = [
+    body('nombre').notEmpty().withMessage('Debes ingresar tu nombre'),
+    body('apellido').notEmpty().withMessage('Debes ingresar tu apellido'),
+    body('correo').notEmpty().withMessage('Debes ingresar un correo valido'),
+    body('contraseña').notEmpty().withMessage('Debes ingresar una contrasenia')
+];
+const validacionesLog = [
+    body('correo').notEmpty().withMessage('Debes ingresar un correo valido'),
+    body('contraseña').notEmpty().withMessage('Debes ingresar una contrasenia')
+];
+/* ////////////////////////////////////////////////////////////////////////////////////////// */
+/* CONFIGURACION MULTER */
+let multerDiskStorageUser = multer.diskStorage({
+    destination: (req, file, cb)=>{
+                    let folder = path.join(__dirname, '../public/images/dbUsers');
+                    cb(null, folder)
+                    
+    },
+    filename:(req, file, cb)=>{
+                    console.log(file);
+                    const imageName = ('imagen' + Date.now() + path.extname(file.originalname))
+                    cb(null, imageName);
+                    
+    }
+})
+let fileUploadUser = multer({storage:multerDiskStorageUser});
+let multerImageMidlewareUser = fileUploadUser.single('imagen')
+
+
+/* //////////////////////////////////////////////////////////////////////// */
+
+
 /* ADMINISTRACION DE RUTAS */
 router.get('/login', userController.login)
-router.get('/registro', userController.registro)
-router.post('/registro', userController.users)
+router.get('/registro',validaciones, userController.registro)
+router.post('/registro',multerImageMidlewareUser, userController.users)
 
 module.exports = router;
