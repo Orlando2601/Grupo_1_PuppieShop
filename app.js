21dcc2af1ca7d023eec2fb71336c7cc4af1bfc40
oswar <oswar.baez26@gmail.com>
2022-02-21 09:47:08 -0500

    [Fix Error Store User]

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const mainRouter = require('./src/routes/mainRouter');/* requerimos archivo de r
 const productosRouter = require('./src/routes/productosRouter');/* requerimos archivo de rutas */
 const userRouter = require('./src/routes/userRouter');/* requerimos archivo de rutas */
 const session = require('express-session');/* requerimos archivo de session */
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
 
 
 /* CONFIGURACION PARA EXTRAER INFORMACION DE LOS FORM///////////////////////////////////////////// */
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-const methodOverride = require('method-override');
-const { cookie } = require('express/lib/response');
-const cookieParser = require('cookie-parser');
+
 app.use(methodOverride("_method"));
 /* /////////////////////////////////////////////////////////////////////////////////////////////// */
 app.set('views', path.resolve(__dirname, './src/views'))
@@ -27,7 +27,7 @@ const puerto = process.env.PORT || 3030
 app.listen(puerto, () => { console.log('Servidor corriendo en el puerto '  + puerto);});/* Configuracion heroku */
 /* //////////////////////////////////////////////////////////////////////////////////// */
 /* CONFIGURACION PARA MIDDLEWARE GENERAL //////////////////////////////////////////////////////////////*/
-app.use(session({secret:'?'}));/* middleware que recorre cada vista */
+app.use(session({secret:'?', resave:false, saveUninitialized:false}));/* middleware que recorre cada vista */
 app.use(cookieParser());
 
 /* //////////////////////////////////////////////////////////////////////////////////// */
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,7 +4,7 @@ const userFilePath = path.join(__dirname, '../data/user.json');
 const usuarios = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
 const { validationResult } = require('express-validator')
 const bcryptjs = require('bcryptjs');
-const { render } = require('express/lib/response');
+
 
 
 const userController ={
@@ -28,35 +28,40 @@ const userController ={
         res.render('users/registro')
     },
     users:(req, res)=>{
-      /* console.log("no hay archivo imagen" + req.file); */
-      const errors = validationResult(req)
-      console.log(errors)
-      let newReference = usuarios.length
-      if(errors.isEmpty()){
-          let userInDB = usuarios.find(user => user.correo == req.body.correo);
-          if(userInDB === undefined && req.file){
-              
-              let nuevoUser = {
-                  id: newReference + 1,
-                  ...req.body,
-                  contraseña: bcryptjs.hashSync(req.body.contraseña, 10),
-                  imagen:req.file.filename
-              }
-              usuarios.push(nuevoUser)
-              fs.writeFileSync(userFilePath, JSON.stringify(usuarios, null, ' '))
-              res.redirect('/user/login') 
-          }else{
-              res.render('users/registro')
-              
-          }
-      }else{ 
-          
-          res.render('users/registro',{
-          
-              errors: errors.mapped(),
-              old: req.body
-          })
-      }
+
+        const errors = validationResult(req)
+        const errores = errors.mapped();
+        console.log("Mensaje body")
+        console.log(req.body.correo)
+        console.log("Mensaje body")
+
+        let newReference = usuarios.length
+        if(errors.isEmpty()){
+            let userInDB = usuarios.find(user => user.correo == req.body.correo);
+            
+            if(userInDB === undefined && req.file){
+                
+                let nuevoUser = {
+                    id: newReference + 1,
+                    ...req.body,
+                    contraseña: bcryptjs.hashSync(req.body.contraseña, 10),
+                    imagen:req.file.filename
+                }
+                usuarios.push(nuevoUser)
+                fs.writeFileSync(userFilePath, JSON.stringify(usuarios, null, ' '))
+                res.redirect('/user/login') 
+            }else{
+                res.render('users/registro')
+                console.log('Usuario registrado')
+            }
+        }else{ 
+            console.log(errores)
+            res.render('users/registro',{
+            
+                errors: errores,
+                old: req.body
+            })
+        }
   },
 
     logged:(req, res)=>{
