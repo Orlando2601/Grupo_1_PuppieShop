bb214f2ceb8f56efb74192ef2e5f2e56069205ba
oswar <oswar.baez26@gmail.com>
2022-03-29 16:00:14 -0500

    [update]

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@
 module.exports={
   "development": {
     "username": "root",
-    "password": "AGUEDa23429944-",
+    "password": "",
     "database": "productosDB",
     "host": "127.0.0.1",
     "dialect": "mysql"
