32c31c7d0c3db2f6c555fe232d723ae3d03243a1
oswar <oswar.baez26@gmail.com>
2022-05-03 14:14:40 -0500

    changes

--- client/src/components/UsuarioItem.jsx
+++ client/src/components/UsuarioItem.jsx
@@ -6,8 +6,11 @@ function UsuarioItem(props){
             <p>Usuario</p>
             <tr className="list-group list-group-flush">
                 <th className="list-group-item">Nombre: {props.nombre}</th>
+                <tr/>
                 <th className="list-group-item">Apellido: {props.apellido}</th>
+                <br/>
                 <th className="list-group-item">Correo: {props.correo}</th>
+                <br/>
                 <a className="list-group-item" href="http://localhost:3030/api/usuario/">Detalle: {props.urlDetalleUser}</a>
             </tr>
 
